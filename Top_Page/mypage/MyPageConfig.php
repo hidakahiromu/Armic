89ccff9ecdccce55a177e1d18880515d6c421c79
maclaichiya <maclaichiya1120@gmail.com>
2020-12-15 23:30:37 +0900

    マイページ
    後投稿ページと削除確認

--- Top_Page/mypage/MyPageConfig.php
+++ Top_Page/mypage/MyPageConfig.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -11,10 +10,10 @@
   <link rel="icon" type="image/x-icon" href="armicicon.png">
   <meta name="description" content="Armic 音楽サイト">
   <meta http-equiv="X-UA=Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width">  
-  
+  <meta name="viewport" content="width=device-width">
+
   <!--Mypageのcss-->
-  <link rel="stylesheet" href="MypageConfig.css">
+  <link rel="stylesheet" href="MyPageConfig.css">
   <!--Mypageのcss-ここまで-->
 </head>
 
@@ -56,66 +55,85 @@
         </a></li>
     </ul>
   </header>
+
   <body>
-   
-    <div class="back_ground">
-        <h1>アーティスト名の取得</h1>
 
-        <div class="post_details">
-            <p>アーティスト名は、音楽の投稿やマイページの機能を使用するために必要となります。他のユーザーと同一のアーティスト名にすることは可能ですが.貴方の音楽を聞く人が他のアーティストが作ったものだと勘違いする可能性があるため、出来るだけ他のユーザーと同じになることの少ないアーティスト名を考えることをお勧めします.<br>※注意 : アーティスト名は変更することが出来ません.<br>以下の項目に必要事項を記入してください.</p>
-           
-        </div>
+    <div class="MyPageConfig_back_ground">
+      <h1>アーティスト名の取得</h1>
 
-        <div class=left_margin><!--文字の左の余白指定-->
+      <div class="MyPageConfig_post_details">
+        <p>アーティスト名は、音楽の投稿やマイページの機能を使用するために必要となります。他のユーザーと同一のアーティスト名にすることは可能ですが.貴方の音楽を聞く人が他のアーティストが作ったものだと勘違いする可能性があるため、出来るだけ他のユーザーと同じになることの少ないアーティスト名を考えることをお勧めします.<br>※注意 : アーティスト名は変更することが出来ません.<br>以下の項目に必要事項を記入してください.</p>
+
+      </div>
+
+      <div class=MyPageConfig_left_margin>
+        <!--文字の左の余白指定-->
         <br><br><br>
 
-            <!--↓action = ""送信先 　method = "post"送信形式?URLに送信内容を表示させない（get内容を表示する）-->
-            <form action="" method="post"　>
-                <p>アーティスト名<p>
-                <div class="form-event">
-                    <label for="name"></label>
-                    <input type="name" name="name" required="required" <!--placeholder="名前"--></input>
-                    <br><br><br>
+        <!--↓action = ""送信先 　method = "post"送信形式?URLに送信内容を表示させない（get内容を表示する）-->
+        <form action="" method="post" 　>
+          <p>アーティスト名<p>
+              <div class>
+                <div class="MyPageConfig_form-event">
+
+                  <label for="name"></label>
+                  <input type="name" name="name" required="required" <!--placeholder="名前" --></input>
+                  <br><br><br>
                 </div>
 
-            
-                
-            </from>
-        </div>
+                <div class=Mypage_post_button_fream>
+                  <div class=Mypage_post_button_rightfream>
+                    <a href="MyPage.php">
+                      <button class="Mypage_Config_rightbutton" type="button" style="width:100%;height:100%">
+                        <p>取得</p>
+                      </button></a></div>
+                  <div class=Mypage_post_button_leftfream>
+                    <a href="MyPage.php">
+                      <button class="Mypage_Config_leftbutton" type="button" style="width:100%;height:100%">
+                        <p>戻る</p>
+                      </button></a></div>
+                </div>
+              </div>
+      </div>
+
+      </from>
+    </div>
     </div>
 
 
 
-  
-  
-    
-  <footer id="footer" class="outer-block">
-    <div class="inner-block">
-      <div class="wrap">
-        <div class="logo">
-          <a href="#"><h1>ARMIC</h1></a>
-        </div>
-        <div class="cont">
-          <div class="c-btn">
-            <a href="#">質問</a>
+
+
+
+    <footer id="footer" class="outer-block">
+      <div class="inner-block">
+        <div class="wrap">
+          <div class="logo">
+            <a href="#">
+              <h1>ARMIC</h1>
+            </a>
           </div>
-          <ul class="nav">
-            <li>ユーザー</li>
-            <li><a href="">ログイン</a></li>
-            <li><a href="">マイページ</a></li>
-          </ul>
-          <ul class="nav">
-            <li>ご利用に際して</li>
-            <li><a href="">Q&A</a></li>
-            <li><a href="">利用規約</a></li>
-          </ul>
+          <div class="cont">
+            <div class="c-btn">
+              <a href="#">質問</a>
+            </div>
+            <ul class="nav">
+              <li>ユーザー</li>
+              <li><a href="">ログイン</a></li>
+              <li><a href="">マイページ</a></li>
+            </ul>
+            <ul class="nav">
+              <li>ご利用に際して</li>
+              <li><a href="">Q&A</a></li>
+              <li><a href="">利用規約</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="copyright">
-      <p><small> © 2020 Armic</small></p>
-    </div>
-  </footer>
-</body>
+      <div class="copyright">
+        <p><small> © 2020 Armic</small></p>
+      </div>
+    </footer>
+  </body>
 
 </html>
